030fe27519495856f0c5b6712d5c337d04c4741f
daanrijnberk <daanrijnberk@hotmail.nl>
2022-03-30 15:05:23 +0200

    Script eruit

---
+++ javascript/maplibre.js
@@ -0,0 +1,6 @@
+var mapi = new maplibregl.Map({
+    container: 'mapi',
+    style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+    center: [-73.545380, 3.507673], // starting position [lng, lat]
+    zoom: 4 // starting zoom
+    });    
---
+++ javascript/openstreet.js
@@ -0,0 +1,12 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([-73.4562148425872, 3.6234758940051988]),
+      zoom: 6
+    })
+  });
