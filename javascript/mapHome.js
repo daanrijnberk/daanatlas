1b696e655b0471bdaa5dc5ce03c6022488f91ff4
daanrijnberk <daanrijnberk@hotmail.nl>
2022-04-14 13:46:59 +0200

    Update mapHome.js

--- javascript/mapHome.js
+++ javascript/mapHome.js
@@ -31,8 +31,10 @@
       }
     ]
   };
+  
 
-  var x = L.geoJSON(myvar).addTo(mymap);   
+  var x = L.geoJSON(myvar).addTo(mymap); 
+    
 
   const geoJsonLayer = L.geoJson().addTo(mymap);
     
@@ -51,3 +53,12 @@
 
 
     }
+
+const wfsLayer = new L.geoJSON();
+wfsLayer.addTo(mymap);
+
+    fetch('https://geoservices.zuid-holland.nl/arcgis/rest/services/Grenzen/Grenzen_regios/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=&returnGeometry=true&maxAllowableOffset=&geometryPrecision=5&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnExceededLimitFeatures=false&quantizationParameters=&returnCentroid=false&sqlFormat=none&resultType=&featureEncoding=esriDefault&datumTransformation=&f=geojson', {})
+.then(response =>json())
+.then(data => {
+  wfsLayer.addData(data);
+});
